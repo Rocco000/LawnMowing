8ae45036812644665132acf7d847c6420fb6e77a
Rocco Iuliano <rocco.iul2000@gmail.com>
2023-12-29 17:15:45 +0100

    Added other layers to the first model

--- gym_game/envs/model.py
+++ gym_game/envs/model.py
@@ -10,8 +10,27 @@ class DeepQNetwork(nn.Module):
         self.input_dim = input_dim[0]*input_dim[0]
         self.n_actions = n_actions
         self.fc1 = nn.Linear(self.input_dim, 128)
+        self.batch1 = nn.BatchNorm1d(128)
+
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, n_actions)
+        self.batch2 = nn.BatchNorm1d(128)
+            
+        self.fc3 = nn.Linear(128, 64)
+        self.batch3 = nn.BatchNorm1d(64)
+            
+        self.fc4 = nn.Linear(64, 64)
+        self.batch4 = nn.BatchNorm1d(64)
+            
+        self.fc5 = nn.Linear(64,32)
+        self.batch5 = nn.BatchNorm1d(32) 
+
+        self.fc6 = nn.Linear(32, n_actions)
+        
+        self.batch6 = nn.BatchNorm1d(n_actions)
+
+        self.my_activation = nn.LeakyReLU(0.1)
+        self.my_drop = nn.Dropout1d(p=0.2)
+
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
         self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
@@ -19,11 +38,20 @@ class DeepQNetwork(nn.Module):
 
     def forward(self, state):
         x = state.view(-1,64) #Transform the game grid in a unidimensional array
-        #print("Dimensione input flattened: ",x.shape)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.fc3(x)
-
+        
+        if x.shape[0] == 1:
+            x = self.my_activation(self.fc1(x))
+            x = self.my_activation(self.fc2(x))
+            x = self.my_activation(self.fc3(x))
+            x = self.my_activation(self.fc4(x))
+            x = self.my_activation(self.fc5(x))
+            x = self.fc6(x)
+        else:
+            x = self.my_activation(self.batch1(self.fc1(x)))
+            x = self.my_activation(self.batch2(self.fc2(x)))
+            x = self.my_drop(self.my_activation(self.batch3(self.fc3(x))))
+            x = self.my_activation(self.batch4(self.fc4(x)))
+            x = self.my_drop(self.my_activation(self.batch5(self.fc5(x))))
+            x = self.batch6(self.fc6(x))
+   
         return x
